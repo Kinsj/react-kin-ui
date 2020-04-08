fafac4330107030e7d623df4562030d2d59e8419
kin <1037671280@qq.com>
2020-04-08 20:50:40 +0800

    首次成功跑起react

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Button from './button';
 
 
-console.log(React);
-console.log(ReactDOM);
+ReactDOM.render(<Button/>, document.body);
