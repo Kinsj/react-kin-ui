10d8f09c60a6274525314f512065a539388d023f
kin <1037671280@qq.com>
2020-04-12 16:51:31 +0800

    让icon 支持所有事件，并简化代码

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import './importIcons';
 import './icon.scss';
+import classes from './helpers/classes';
 
 // 申明Icon 组件接受的props属性的类型
-interface IconProps {
+interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string,
-  // onClick: () => void  // 这种写法的弊端就是如果传参，就得指定参数类型，如果访问参数内的属性，又得指定参数内属性的类型，
-  // 参数不确定还得断言，根本没法写
-  // 所以还是采用下面这种写法
-
-  onClick: React.MouseEventHandler<SVGElement>
 }
 
 // 阅读FunctionComponent源码可见，类型定义里 FunctionComponent 接受一个interface参数指定props类型，并接受children作为可选参数
-const Icon: React.FunctionComponent<IconProps> = (props) => {
+const Icon: React.FunctionComponent<IconProps> =
+  ({
+     className,
+     name,
+     ...restProps
+  }) => {
+  // 这里Icon组件是一个SVG元素，所以相对HTMLAttributes 更应该用 SVGAttributes
   return (
-    <svg className="gulu-icon" onClick={props.onClick}>
-      <use xlinkHref={"#" + props.name}/>
+    <svg className={classes("gulu-icon", className)}
+         {...restProps}>
+      <use xlinkHref={"#" + name}/>
     </svg>
   );
 };
