117e67a81a5f3e94e9920d39dc0ff113fefed925
kin <1037671280@qq.com>
2020-04-12 09:29:45 +0800

    添加 onClick 事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -4,13 +4,15 @@ import './icon.scss';
 
 // 申明Icon 组件接受的props属性的类型
 interface IconProps {
-  name: string
+  name: string,
+  onClick: () => void
+  // onClick: React.MouseEventHandler<SVGElement>
 }
 
 // 阅读FunctionComponent源码可见，类型定义里 FunctionComponent 接受一个interface参数指定props类型，并接受children作为可选参数
 const Icon: React.FunctionComponent<IconProps> = (props) => {
   return (
-    <svg className="gulu-icon">
+    <svg className="gulu-icon" onClick={props.onClick}>
       <use xlinkHref={"#" + props.name}/>
     </svg>
   );
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Icon from './icon';
 
-ReactDOM.render(<div>
-    <Icon name="alipay"/>
-    <Icon name="qq"/>
-    <Icon name="wechat"/>
+ReactDOM.render(
+  <div>
+    <Icon name="qq" onClick={()=>{console.log(1)}}/>
   </div>
   , document.querySelector('#root'));
