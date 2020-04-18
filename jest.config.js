ccaf593ed35701e313d479e0e1ef74dc1350f3b7
kin <1037671280@qq.com>
2020-04-18 20:08:01 +0800

    区分 test 和 ci 的配置文件

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,9 @@
 // https://jestjs.io/docs/en/configuration.html
-
 module.exports = {
   verbose: true,
   clearMocks: false,
-  reporters: ['default', 'jest-junit'],  // 配置分析报表
-
-  // 覆盖率相关配置
-  collectCoverage: true,  // 是否生成测试报表
-  collectCoverageFrom: ['lib/**/*.{ts,tsx}', '!**/node_modules/**'],
-  coverageDirectory: 'coverage',  // 生成的报告存放路径
-  coverageReporters: ['text', 'lcov'],  // 生成报告的种类： text: 控制台输出， lcov: 业内知名分析报告
-
+  reporters: ['default'],  // 配置分析报表
+  collectCoverage: false,  // 是否生成测试报表
 
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleNameMapper: {  // 将以下对应后缀的文件默认导出为指定格式（__mocks__目录下的js指定）
