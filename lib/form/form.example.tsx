9e74705677efaa391114cf4d55ef9fedfd704627
kin <1037671280@qq.com>
2020-05-12 21:23:51 +0800

    实现 Form 基本功能

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -1,17 +1,29 @@
-import React, {useState} from 'react';
-import Form from './form';
+import React, {Fragment, useState} from 'react';
+import Form, {FormValue} from './form';
 
 const FormExample: React.FunctionComponent = () => {
-  const [formData] = useState({
-    username: '',
-    password: ''
+  const [formData, setFormData] = useState<FormValue>({
+    username: 'kin',
+    password: '123'
   });
   const [fields] = useState([
     {name: 'username', label: '用户名', input: {type: 'text'}},
     {name: 'password', label: '密码', input: {type: 'password'}}
   ]);
+  const onSubmitHandle: React.FormEventHandler<HTMLFormElement> = () => {
+    console.log(formData);
+  };
   return (
-    <Form value={formData} fields={fields}/>
+    <Form value={formData} fields={fields}
+          buttons={
+            <Fragment>
+              <button type="submit">提交</button>
+              <button>取消</button>
+            </Fragment>
+          }
+          onSubmit={onSubmitHandle}
+          onChange={setFormData}
+    />
   );
 };
 
--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, {ReactFragment} from 'react';
+
+export interface FormValue {
+  [K: string]: any
+}
+
 
 interface Props {
-  value: { [K: string]: any };
+  value: FormValue;
   fields: Array<{ name: string, label: string, input: { type: string } }>;
+  buttons: ReactFragment,
+  onChange: (value: FormValue) => void,
+  onSubmit: React.FormEventHandler<HTMLFormElement>
 }
 
 const Form: React.FunctionComponent<Props> = (props) => {
+  const formData = props.value;
+  const onInputChangeHandle
+    = (name: string, e: React.ChangeEvent<HTMLInputElement>) => {
+    props.onChange({...formData, [name]: e.target.value});
+  };
+  const onSubmitHandle: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault(); // 阻止默认submit事件执行
+    props.onSubmit(e);
+  };
   return (
-    <div>
+    <form onSubmit={onSubmitHandle}>
       {props.fields.map(f =>
-        <div>
+        <div key={f.name}>
           {f.label}
+          <input type={f.input.type} value={formData[f.name]}
+                 onChange={onInputChangeHandle.bind(null, f.name)
+                   /*这里使用bind可以把f.name作为前置参数传给onInputChangeHandle
+                   * 相当于调用了 onInputChangeHandle(f.name, event)*/
+                 }/>
         </div>
       )}
-    </div>
+      <div>
+        {props.buttons}
+      </div>
+    </form>
   );
 };
 
